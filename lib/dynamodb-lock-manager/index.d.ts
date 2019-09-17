fd7385f43b2b728a77bc8df1c919790ff11fdf88
RJLyders <richard@lyders.com>
2019-09-16 22:43:24 -0500

    removed lib dir of generated files from git

--- lib/dynamodb-lock-manager/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { DynamoDBLockManager } from './dynamodb-lock-manager';
-export { DynamoDBLock } from './dynamodb-lock';
--- lib/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { DynamoDBLockManager, DynamoDBLock } from './dynamodb-lock-manager';
